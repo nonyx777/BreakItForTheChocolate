79ae2bf0d34ca9612e0de9aef91b4d3034119c1f
nonyx777 <nahomretebo@gmail.com>
2025-06-07 00:32:51 +0300

    Creature Interaction with Smashable Object, Object breaks when fallen to ground

---
+++ Script/DestructibleObjects/smashObject.gd
@@ -0,0 +1,32 @@
+extends RigidBody3D
+
+@onready var stm_instance = $STMCachedInstance3D
+
+var smash_once: bool = true
+
+func _ready():
+	contact_monitor = true
+	max_contacts_reported = 50
+
+func smash() -> void:
+	if !stm_instance:
+		return
+	# Break
+	stm_instance.smash_the_mesh()
+	# Apply an "explode" impulse to each fragment/chunk
+	# Define a callback to apply an impulse to a rigid body chunk
+	var explode_callback = func(rb: RigidBody3D, _from):
+		rb.call_deferred("apply_impulse", -rb.position.normalized() * Vector3(1, -1, 1) * 3.0)
+	# Apply the callback to each chunk of the mesh
+	stm_instance.chunks_iterate(explode_callback)
+
+
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	for i in state.get_contact_count():
+		var collider = state.get_contact_collider_object(i)
+		if collider and collider is CollisionObject3D:
+			if (collider.collision_layer & (1 << 1)) != 0:  # Layer 2 = bit index 1
+				if smash_once:
+					smash()
+					smash_once = false
