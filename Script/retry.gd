0652c5a0c43016d686a2203a4ccd0ae05c5eba8e
nonyx777 <nahomretebo@gmail.com>
2025-06-07 23:14:00 +0300

    minor changes and object relocation

--- Script/retry.gd
+++ Script/retry.gd
@@ -6,7 +6,7 @@ func _on_retry_pressed() -> void:
 	# This delay reduces the possiblity of smashed objects still existing after retry
 	# I think It gives the STM plugn time to clean stuff up
 	"""
-	await get_tree().create_timer(2).timeout
+	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://Scene/living_room.tscn")
 
 func _on_quit_pressed() -> void:
