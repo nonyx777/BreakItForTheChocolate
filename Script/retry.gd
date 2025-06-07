25227a8a4b47b163082c8fb74034834a6ec4ac8e
nonyx777 <nahomretebo@gmail.com>
2025-06-07 23:02:18 +0300

    minor fixed in retry scene

--- Script/retry.gd
+++ Script/retry.gd
@@ -2,6 +2,11 @@ extends Control
 
 
 func _on_retry_pressed() -> void:
+	"""
+	# This delay reduces the possiblity of smashed objects still existing after retry
+	# I think It gives the STM plugn time to clean stuff up
+	"""
+	await get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file("res://Scene/living_room.tscn")
 
 func _on_quit_pressed() -> void:
