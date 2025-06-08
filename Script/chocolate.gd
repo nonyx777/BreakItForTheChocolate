b8ffca3a7d48c21c21b40aa4eac6a73b32c7d0cf
nonyx777 <nahomretebo@gmail.com>
2025-06-08 20:24:46 +0300

    pulsing light for the chocolate

--- Script/chocolate.gd
+++ Script/chocolate.gd
@@ -2,6 +2,8 @@ extends Area3D
 
 @onready var signalManager = get_parent().get_parent().get_node("SignalManager")
 @onready var chocolateMesh = $Chocolate
+@onready var winnerLabel: Label = get_parent().get_parent().get_node("Label")
+
 var able_to_take_chocolate: bool = false
 
 func ableToTakeChocolate():
@@ -9,9 +11,11 @@ func ableToTakeChocolate():
 
 func _ready():
 	signalManager.connect("all_object_broken", ableToTakeChocolate)
+	winnerLabel.visible = false
 
 func _on_body_entered(body: Node3D) -> void:
-	signalManager.disconnect("all_object_broken", ableToTakeChocolate)
 	if able_to_take_chocolate:
+		signalManager.disconnect("all_object_broken", ableToTakeChocolate)
+		winnerLabel.visible = true
 		chocolateMesh.queue_free()
 		print("You took the chocolate!")
