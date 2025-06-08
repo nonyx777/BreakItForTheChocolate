486eca0205b2563dde01610ac05b72c191b3ba53
nonyx777 <nahomretebo@gmail.com>
2025-06-08 12:08:25 +0300

    minor changes

--- Script/menu.gd
+++ Script/menu.gd
@@ -2,7 +2,13 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scene/living_room.tscn")
+	call_deferred("changeScene")
 
 func _on_quit_pressed() -> void:
+	call_deferred("quitScene")
+
+func changeScene():
+	get_tree().change_scene_to_file("res://Scene/living_room.tscn")
+
+func quitScene():
 	get_tree().quit()
--- Script/retry.gd
+++ Script/retry.gd
@@ -5,7 +5,10 @@ func _on_retry_pressed() -> void:
 	call_deferred("changeScene")
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	call_deferred("quitScene")
 
 func changeScene():
 	get_tree().change_scene_to_file("res://Scene/living_room.tscn")
+
+func quitScene():
+	get_tree().quit()
